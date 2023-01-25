00cc5f32da9f5e18600996790c21ac029aff3357
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-01-26 00:12:49 +0530

    updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -11,12 +11,33 @@ class Configuration:
         config_file_path:str =CONFIG_FILE_PATH,
         current_time_stamp: str = CURRENT_TIME_STAMP
         ) -> None:
-        self.config_info = read_yaml_file(file_path=config_file_path)
-        self.training_pipeline_config = self.get_training_pipeline_config()
-        self.time_stamp = current_time_stamp
+        try:
+            self.config_info = read_yaml_file(file_path=config_file_path)
+            self.training_pipeline_config = self.get_training_pipeline_config()
+            self.time_stamp = current_time_stamp
+        except Exception as e:
+            raise HousingException(e,sys) from e
     
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-        pass
+        try:
+            self.config_info
+            dataset_download_url
+            tgz_download_dir
+            raw_data_dir
+            ingested_train_dir,
+            ingested_test_dir,
+            data_ingestion_config = DataIngestionConfig(
+                dataset_download_url=dataset_download_url,
+                tgz_download_dir=tgz_download_dir,
+                raw_data_dir=raw_data_dir,
+                ingested_train_dir=ingested_train_dir,
+                ingested_test_dir=ingested_test_dir
+            )
+            logging.info(f"Data Ingestion Config: {data_ingestion_config}")
+            return data_ingestion_config
+        except Exception as e:
+            raise HousingException(e,sys) from e
+            
     
     def get_data_validation_config(self) -> DataValidationConfig:
         pass
