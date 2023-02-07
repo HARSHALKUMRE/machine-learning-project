9870e96c34d3c10186fa2372aa84c533566c0567
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-02-07 18:58:22 +0530

    updated

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ from housing.pipeline.training_pipeline import TrainingPipeline
 from housing.exception import HousingException
 from housing.logger import logging
 from housing.config.configuration import Configuration
+from housing.component.data_transformation import DataTransformation
 
 def main():
     try:
@@ -10,8 +11,13 @@ def main():
         #data_validation_config = Configuration().get_data_validation_config()
         #print(data_validation_config)
         
-        data_transformation_config = Configuration().get_data_transformation_config()
-        print(data_transformation_config)
+        #data_transformation_config = Configuration().get_data_transformation_config()
+        #print(data_transformation_config)
+        schema_file_path = r"G:\100-days-of-dl\Krish Naik\FSDS Ineuron Course\projects\machine-learning-project\config\schema.yaml"
+        file_path = r"G:\100-days-of-dl\Krish Naik\FSDS Ineuron Course\projects\machine-learning-project\housing\artifact\data_ingestion\2023-02-01-11-42-50\ingested_data\train\housing.csv"
+        df = DataTransformation.load_data(file_path=file_path, schema_file_path=schema_file_path)
+        print(df.columns)
+        print(df.dtypes)
         
     except Exception as e:
         logging.error(f"{e}")
--- housing/utils/main_utils.py
+++ housing/utils/main_utils.py
@@ -1,6 +1,8 @@
 import yaml
 from housing.exception import HousingException
 import os,sys
+import numpy as np
+import dill
 
 def read_yaml_file(file_path:str) -> dict:
     """
@@ -14,4 +16,45 @@ def read_yaml_file(file_path:str) -> dict:
     except Exception as e:
         raise HousingException(e,sys) from e
     
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
     
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise HousingException(e, sys) from e
+    
+    
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file   
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    
+    try:
+        with open(file_path, 'rb') as file_obj:
+            
+            return np.load(file_obj)
+    except Exception as e:
+        raise HousingException(e, sys) from e
+    
+def save_object(file_path: str, obj):
+    """
+    file_path: str
+    obj: Any sort of object
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise HousingException(e, sys) from e
